bbed274d67cad4e2525bc1fb775a41cd608f2edf
Filippo Bonafini <filippo.bonafini@outlook.it>
2023-03-27 10:16:13 +0200

    feat: starting scaffolding

---
+++ src/components/HelloApp.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    name: 'Hello!'
+}
+</script>
+
+<template>
+    <div>
+        <h1>Hello world!</h1>
+    </div>
+</template>
+
+
+<style lang="scss" scoped></style>
